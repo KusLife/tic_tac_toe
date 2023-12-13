9574229ed2c29185a0a5edfcd37080c8f574626b
Kus <larisovkirill@gmail.com>
2023-12-13 22:51:09 +0100

    added triplet fnc, improved plr arr,

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tic Tac Toe</title>
-</head>
-<body>
-  
-</body>
-</html>
+const grid_container = document.querySelector('#grid_container');
+const grid_Evl = grid_container.addEventListener('click', (event) => {
+  const chosenId = event.target.id;
+
+  // console.log(event.target.id);
+  chosenArea(chosenId);
+});
+
+for (let i = 0; i <= 8; i++) {
+  grid_container.innerHTML += `<div class='grid-item ' id='item-${i}'></div>`;
+}
+
+const playerX = 'x';
+const playerO = 'o';
+let turn = playerX;
+
+function chosenArea(id) {
+  let grid_item_chosen = document.getElementById(`${id}`);
+
+  if (grid_item_chosen.innerText === '') {
+    console.log('empty');
+    if (turn === playerO) {
+      // grid_item_chosen.innerHTML = `<p>${playerX}</p>`;
+      grid_item_chosen.innerHTML = playerX;
+      turn = playerX;
+    } else {
+      // grid_item_chosen.innerHTML = `<p>${playerO}</p>`;
+      grid_item_chosen.innerText = playerO;
+      turn = playerO;
+    }
+  } else {
+    console.log('buisy');
+  }
+}
